98be7347b5db005bd66e85c25dd1551c820384d7
Abraham Furlan <abrahamfurlan@gmail.com>
2024-03-05 13:23:19 -0500

    star name generator

---
+++ common/starNames.js
@@ -0,0 +1,24 @@
+const starPrefix = [
+    "starry",
+    "shiny",
+    "glowing",
+    "soft",
+    "cool",
+    "hot",
+    "radiant",
+    "stellar",
+    "astral"
+];
+
+const starSuffix = [
+    "star",
+    "supernova",
+    "stardust",
+    "object",
+    "comet"
+];
+
+module.exports = {
+    starPrefix,
+    starSuffix
+};
